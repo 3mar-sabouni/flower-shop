6674f9769cfd2bca2d9ec602f81ea6c0db86e7fa
3mar-sabouni <3marsabouni@gmail.com>
2024-04-21 14:13:05 +0300

    part 1

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "flower shop",
+
+    'summary': "",
+
+    'description': """
+    """,
+
+    'author': "Omar Sabouni",
+    'website': "",
+    'category': 'Uncategorized',
+    'version': '17.0.0.1',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/flower_flower_views.xml'
+    ],
+    'demo': [
+    ],
+}
+
