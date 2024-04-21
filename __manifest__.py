97fa13925e18448523da6e00a9051e8a009a2097
3mar-sabouni <3marsabouni@gmail.com>
2024-04-21 16:16:43 +0300

    Part 3

--- __manifest__.py
+++ __manifest__.py
@@ -10,13 +10,14 @@
     'author': "Omar Sabouni",
     'website': "",
     'category': 'Uncategorized',
-    'version': '17.0.0.1',
-    'depends': ['base','sale'],
+    'version': '17.0.0.2',
+    'depends': ['base','sale','website_sale'],
     'data': [
         'security/ir.model.access.csv',
         'views/flower_flower_views.xml',
-        'views/product_product_views.xml'
-    ],
+        'views/product_product_views.xml',
+        'views/template_product_inhert.xml'
+        ],
     'demo': [
     ],
 }
