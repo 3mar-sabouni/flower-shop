081fa6cc1365a7ba57e01f2c5f3a19de341c6dd5
3mar-sabouni <3marsabouni@gmail.com>
2024-04-28 16:53:23 +0300

    Part 6

--- __manifest__.py
+++ __manifest__.py
@@ -13,6 +13,7 @@
     'version': '17.0.0.2',
     'depends': ['base','sale','website_sale','stock'],
     'data': [
+        'security/groups.xml',
         'security/ir.model.access.csv',
         'views/flower_water_views.xml',
         'views/flower_flower_views.xml',
@@ -20,6 +21,7 @@
         'views/template_product_inhert.xml',
         'views/stock_production_lot_inhert.xml',
         'reports/flower_sale_order_views.xml',
+        'data/ir_cron.xml',
         ],
     'demo': [
     ],
